MEINE-ERDE/micropython

commit 893b857f3ed681e0d984f9628766d85ca9c52b79

ports: Make all network-capable boards use bundle-networking.
This will ensure that any board with networking support gets:
 - webrepl
 - mip
 - urequests
 - ntptime

This work was funded through GitHub Sponsors.

Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.mk
@@ -21,3 +21,5 @@ MICROPY_PY_LWIP = 1
 MICROPY_PY_USSL = 1
 MICROPY_SSL_MBEDTLS = 1
 MICROPY_VFS_LFS2 = 1
+
+FROZEN_MANIFEST = $(BOARD_DIR)/manifest.py

ports/mimxrt/boards/MIMXRT1020_EVK/mpconfigboard.mk
@@ -13,6 +13,8 @@ MICROPY_PY_LWIP = 1
 MICROPY_PY_USSL = 1
 MICROPY_SSL_MBEDTLS = 1
 
+FROZEN_MANIFEST ?= $(BOARD_DIR)/manifest.py
+
 JLINK_PATH ?= /media/RT1020-EVK/
 JLINK_COMMANDER_SCRIPT = $(BUILD)/script.jlink
 

ports/mimxrt/boards/MIMXRT1050_EVK/mpconfigboard.mk
@@ -12,3 +12,5 @@ MICROPY_HW_SDRAM_SIZE  = 0x2000000  # 32MB
 MICROPY_PY_LWIP = 1
 MICROPY_PY_USSL = 1
 MICROPY_SSL_MBEDTLS = 1
+
+FROZEN_MANIFEST ?= $(BOARD_DIR)/manifest.py

ports/mimxrt/boards/MIMXRT1060_EVK/mpconfigboard.mk
@@ -13,6 +13,8 @@ MICROPY_PY_LWIP = 1
 MICROPY_PY_USSL = 1
 MICROPY_SSL_MBEDTLS = 1
 
+FROZEN_MANIFEST ?= $(BOARD_DIR)/manifest.py
+
 JLINK_PATH ?= /media/RT1060-EVK/
 JLINK_COMMANDER_SCRIPT = $(BUILD)/script.jlink
 

ports/mimxrt/boards/MIMXRT1064_EVK/mpconfigboard.mk
@@ -13,6 +13,8 @@ MICROPY_PY_LWIP = 1
 MICROPY_PY_USSL = 1
 MICROPY_SSL_MBEDTLS = 1
 
+FROZEN_MANIFEST ?= $(BOARD_DIR)/manifest.py
+
 JLINK_PATH ?= /media/RT1064-EVK/
 
 deploy: $(BUILD)/firmware.bin

ports/mimxrt/boards/MIMXRT1170_EVK/mpconfigboard.mk
@@ -15,6 +15,8 @@ MICROPY_PY_LWIP = 1
 MICROPY_PY_USSL = 1
 MICROPY_SSL_MBEDTLS = 1
 
+FROZEN_MANIFEST ?= $(BOARD_DIR)/manifest.py
+
 CFLAGS += -DCPU_MIMXRT1176DVMAA_cm7 \
 		-DMIMXRT117x_SERIES \
 		-DENET_ENHANCEDBUFFERDESCRIPTOR_MODE=1 \

ports/mimxrt/boards/SEEED_ARCH_MIX/mpconfigboard.mk
@@ -13,4 +13,6 @@ MICROPY_PY_LWIP = 1
 MICROPY_PY_USSL = 1
 MICROPY_SSL_MBEDTLS = 1
 
+FROZEN_MANIFEST ?= $(BOARD_DIR)/manifest.py
+
 CFLAGS += -DSPI_RETRY_TIMES=1000000

ports/mimxrt/boards/TEENSY41/mpconfigboard.mk
@@ -11,5 +11,7 @@ MICROPY_PY_LWIP = 1
 MICROPY_PY_USSL = 1
 MICROPY_SSL_MBEDTLS = 1
 
+FROZEN_MANIFEST ?= $(BOARD_DIR)/manifest.py
+
 deploy: $(BUILD)/firmware.hex
 	teensy_loader_cli --mcu=imxrt1062 -v -w $<

commit b81d2ae239ef8fcac3a8eeb9e29568cd3f5f308f

stm32/boards/NUCLEO_H743ZI2: Refer to NUCLEO_H743ZI for frozen manifest.
Fixes build issue introduced by 893b857f3ed681e0d984f9628766d85ca9c52b79

Signed-off-by: Damien George <damien@micropython.org>

ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.mk
@@ -22,4 +22,4 @@ MICROPY_PY_USSL = 1
 MICROPY_SSL_MBEDTLS = 1
 MICROPY_VFS_LFS2 = 1
 
-FROZEN_MANIFEST = $(BOARD_DIR)/manifest.py
+FROZEN_MANIFEST ?= $(BOARD_DIR)/manifest.py

ports/stm32/boards/NUCLEO_H743ZI2/mpconfigboard.mk
@@ -1,1 +1,3 @@
+FROZEN_MANIFEST ?= boards/NUCLEO_H743ZI/manifest.py
+
 include boards/NUCLEO_H743ZI/mpconfigboard.mk