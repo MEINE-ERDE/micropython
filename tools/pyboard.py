MEINE-ERDE/micropython

commit 6d278bef6fea68350aa4a9d98967f434bded1e1f

tools/pyboard.py: Use '/' exclusively when dealing with paths.
Currently, certain mpremote filesystem operations can fail on Windows due
to a mixing of '/' and '\' for path separators.  Eg if filesystem_command()
is called with a destination that ends in / then dest.endswith(os.path.sep)
will return False, which gives the wrong behaviour (it does end in a path
separator).

For similar reasons to b5c11bd1ec07a71cc9ff9c9cd87d1531805c95b6, it's best
to use '/' everywhere in pyboard.py and mpremote, because the target device
understands only '/'.  mpremote already does this, so the remaining place
to fix it is in pyboard.y, to convert all incoming paths to use '/' instead
of '\'.

This effectively reverts 034c8bbaaaf70f3d0dfe31151af3980890948412 which
tried to fix the problem in a different way.

See also related 999e2ccc3a27390ed1105106f0b11a949c30eacd.

Signed-off-by: Damien George <damien@micropython.org>

tools/pyboard.py
@@ -663,15 +663,16 @@ def filesystem_command(pyb, args, progress_callback=None, verbose=False):
     def fname_remote(src):
         if src.startswith(":"):
             src = src[1:]
-        return src
+        # Convert all path separators to "/", because that's what a remote device uses.
+        return src.replace(os.path.sep, "/")
 
     def fname_cp_dest(src, dest):
         _, src = os.path.split(src)
         if dest is None or dest == "":
             dest = src
         elif dest == ".":
-            dest = os.path.join(".", src)
-        elif dest.endswith(os.path.sep):
+            dest = "/".join(".", src)
+        elif dest.endswith("/"):
             dest += src
         return dest
 

commit df7da621de7bed7e85a027a40d1119ea6ed12760

tools/pyboard.py: Fix joining of path in filesystem_command.
This was broken by 6d278bef6fea68350aa4a9d98967f434bded1e1f

Signed-off-by: Damien George <damien@micropython.org>

tools/pyboard.py
@@ -671,7 +671,7 @@ def filesystem_command(pyb, args, progress_callback=None, verbose=False):
         if dest is None or dest == "":
             dest = src
         elif dest == ".":
-            dest = "/".join(".", src)
+            dest = "./" + src
         elif dest.endswith("/"):
             dest += src
         return dest