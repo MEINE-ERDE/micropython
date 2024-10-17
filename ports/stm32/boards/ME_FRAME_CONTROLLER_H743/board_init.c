d8348bd075db5d41c07a5ef33b6b1f969e920382
fabianlefebre <117187889+fabianlefebre@users.noreply.github.com>
2024-10-17 11:30:16 +0200

    add our custom board

---
+++ ports/stm32/boards/ME_FRAME_CONTROLLER_H743/board_init.c
@@ -0,0 +1,13 @@
+#include "py/mphal.h"
+
+// This init function is needed to memory map the QSPI flash early in the boot process
+
+void board_early_init(void)
+{
+    // Turn off the USB switch
+    mp_hal_pin_output(pyb_pin_OTG_FS_POWER);
+    mp_hal_pin_low(pyb_pin_OTG_FS_POWER);
+
+    mp_hal_pin_output(pyb_pin_H1);
+    mp_hal_pin_high(pyb_pin_H1);
+}
---
+++ ports/stm32/boards/ME_FRAME_CONTROLLER_H743/mpconfigboard.h
@@ -0,0 +1,167 @@
+#define MICROPY_HW_BOARD_NAME       "ME_FRAME_CONTROLLER_H743"
+#define MICROPY_HW_MCU_NAME         "STM32H743"
+
+#define MICROPY_PY_PYB_LEGACY       (0)
+
+#define MICROPY_HW_ENABLE_RTC       (1)
+#define MICROPY_HW_ENABLE_RNG       (1)
+#define MICROPY_HW_ENABLE_ADC       (1)
+#define MICROPY_HW_ENABLE_DAC       (1)
+#define MICROPY_HW_ENABLE_USB       (1)
+#define MICROPY_HW_ENABLE_SDCARD    (0)
+#define MICROPY_HW_HAS_SWITCH       (1)
+#define MICROPY_HW_HAS_FLASH        (1)
+
+// ADDED FL TO ACTIVATE SPI FLASH 
+// Flash storage config
+#define MICROPY_HW_SPIFLASH_ENABLE_CACHE            (1)
+#define MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE    (0)
+
+// The board has a 25 MHz external clock source, resulting in 100 MHz CPU speed
+#define MICROPY_HW_CLK_PLLM         (2)
+#define MICROPY_HW_CLK_PLLN         (16)
+#define MICROPY_HW_CLK_PLLP         (2)
+#define MICROPY_HW_CLK_PLLQ         (2) 
+#define MICROPY_HW_CLK_PLLR         (2)
+#define MICROPY_HW_CLK_PLLVCI       (RCC_PLL1VCIRANGE_1)
+#define MICROPY_HW_CLK_PLLVCO       (RCC_PLL1VCOWIDE)
+#define MICROPY_HW_CLK_PLLFRAC      (0)
+// #define MICROPY_HW_CLK_PLLM         (4)
+// #define MICROPY_HW_CLK_PLLN         (480)
+// #define MICROPY_HW_CLK_PLLP         (2)
+// #define MICROPY_HW_CLK_PLLQ         (4)
+// #define MICROPY_HW_CLK_PLLR         (2)
+// #define MICROPY_HW_CLK_PLLVCI       (RCC_PLL1VCIRANGE_1)
+// #define MICROPY_HW_CLK_PLLVCO       (RCC_PLL1VCOWIDE)
+// #define MICROPY_HW_CLK_PLLFRAC      (0)
+
+// PLL2 75 MHz, unused
+#define MICROPY_HW_CLK_PLL2M            (2)
+#define MICROPY_HW_CLK_PLL2N            (12)
+#define MICROPY_HW_CLK_PLL2P            (2)
+#define MICROPY_HW_CLK_PLL2Q            (2)
+#define MICROPY_HW_CLK_PLL2R            (2)
+#define MICROPY_HW_CLK_PLL2VCI          (RCC_PLL2VCIRANGE_2)
+#define MICROPY_HW_CLK_PLL2VCO          (RCC_PLL2VCOWIDE)
+#define MICROPY_HW_CLK_PLL2FRAC         (0)
+
+// The USB clock is set using PLL3, resulting in 48 MHz
+#define MICROPY_HW_CLK_PLL3M        (5)
+#define MICROPY_HW_CLK_PLL3N        (48)
+#define MICROPY_HW_CLK_PLL3P        (2)
+#define MICROPY_HW_CLK_PLL3Q        (5) // -> 48 MHz
+#define MICROPY_HW_CLK_PLL3R        (2)
+#define MICROPY_HW_CLK_PLL3VCI      (RCC_PLL3VCIRANGE_1)
+#define MICROPY_HW_CLK_PLL3VCO      (RCC_PLL3VCOWIDE)
+#define MICROPY_HW_CLK_PLL3FRAC     (0)
+
+// 4 wait states
+#define MICROPY_HW_FLASH_LATENCY    FLASH_LATENCY_4
+
+// UART config
+#define MICROPY_HW_UART2_TX         (pin_D5)
+#define MICROPY_HW_UART2_RX         (pin_D6)
+#define MICROPY_HW_UART2_RTS        (pin_D4)
+#define MICROPY_HW_UART2_CTS        (pin_D3)
+#define MICROPY_HW_UART3_TX         (pin_D8)
+#define MICROPY_HW_UART3_RX         (pin_D9)
+// #define MICROPY_HW_UART5_TX         (pin_B6)
+// #define MICROPY_HW_UART5_RX         (pin_B12)
+#define MICROPY_HW_UART6_TX         (pin_C6)
+#define MICROPY_HW_UART6_RX         (pin_C7)
+#define MICROPY_HW_UART7_TX         (pin_F7)
+#define MICROPY_HW_UART7_RX         (pin_F6)
+// #define MICROPY_HW_UART8_TX         (pin_E1)
+// #define MICROPY_HW_UART8_RX         (pin_E0)
+
+#define MICROPY_HW_UART_REPL        PYB_UART_3
+#define MICROPY_HW_UART_REPL_BAUD   115200
+
+// I2C buses
+#define MICROPY_HW_I2C1_SCL         (pin_B8)
+#define MICROPY_HW_I2C1_SDA         (pin_B9)
+#define MICROPY_HW_I2C2_SCL         (pin_F1)
+#define MICROPY_HW_I2C2_SDA         (pin_F0)
+#define MICROPY_HW_I2C4_SCL         (pin_F14)
+#define MICROPY_HW_I2C4_SDA         (pin_F15)
+
+// SPI buses
+#define MICROPY_HW_SPI3_NSS         (pin_A4)
+#define MICROPY_HW_SPI3_SCK         (pin_B3)
+#define MICROPY_HW_SPI3_MISO        (pin_B4)
+#define MICROPY_HW_SPI3_MOSI        (pin_B5)
+
+// USRSW is pulled low. Pressing the button makes the input go high.
+#define MICROPY_HW_USRSW_PIN        (pin_C13)
+#define MICROPY_HW_USRSW_PULL       (GPIO_NOPULL)
+#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_RISING)
+#define MICROPY_HW_USRSW_PRESSED    (1)
+
+// LEDs
+#define MICROPY_HW_LED1             (pin_E3)    // green'
+// #define MICROPY_HW_LED2             (pin_B7)    // blue
+// #define MICROPY_HW_LED3             (pin_B14)   // red
+#define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_high(pin))
+#define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_low(pin))
+
+// USB config
+#define MICROPY_HW_USB_FS              (1)
+#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
+#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
+
+// FDCAN bus
+#define MICROPY_HW_CAN1_NAME  "FDCAN1"
+#define MICROPY_HW_CAN1_TX    (pin_D1)
+#define MICROPY_HW_CAN1_RX    (pin_D0)
+
+// SD card detect switch
+// #define MICROPY_HW_SDCARD_DETECT_PIN        (pin_G2)
+// #define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
+// #define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
+
+// Ethernet via RMII
+#define MICROPY_HW_ETH_MDC          (pin_C1)
+#define MICROPY_HW_ETH_MDIO         (pin_A2)
+#define MICROPY_HW_ETH_RMII_REF_CLK (pin_A1)
+#define MICROPY_HW_ETH_RMII_CRS_DV  (pin_A7)
+#define MICROPY_HW_ETH_RMII_RXD0    (pin_C4)
+#define MICROPY_HW_ETH_RMII_RXD1    (pin_C5)
+#define MICROPY_HW_ETH_RMII_TX_EN   (pin_B11)
+#define MICROPY_HW_ETH_RMII_TXD0    (pin_B12)
+#define MICROPY_HW_ETH_RMII_TXD1    (pin_B13)
+
+
+// // QSPI flash #1 for storage
+// #define MICROPY_HW_QSPI_PRESCALER           (2) // 100MHz
+// #define MICROPY_HW_QSPIFLASH_SIZE_BITS_LOG2 (27) // 16 mbyte
+// // Reserve 1MiB at the end for compatibility with alternate firmware that places WiFi blob here.
+// #define MICROPY_HW_SPIFLASH_SIZE_BITS       (128 * 1024 * 1024)
+// #define MICROPY_HW_QSPIFLASH_CS             (pin_G6)
+// #define MICROPY_HW_QSPIFLASH_SCK            (pin_B2)
+// #define MICROPY_HW_QSPIFLASH_IO0            (pin_D11)
+// #define MICROPY_HW_QSPIFLASH_IO1            (pin_D12)
+// #define MICROPY_HW_QSPIFLASH_IO2            (pin_E2)
+// #define MICROPY_HW_QSPIFLASH_IO3            (pin_D13)
+
+// #define MICROPY_HW_RCC_QSPI_CLKSOURCE   (RCC_QSPICLKSOURCE_PLL2)
+
+#if (MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE == 0)
+// QSPI flash #1 for storage
+#define MICROPY_HW_QSPI_PRESCALER           (1) // 100MHz
+#define MICROPY_HW_QSPIFLASH_SIZE_BITS_LOG2 (27)
+#define MICROPY_HW_SPIFLASH_SIZE_BITS       (128 * 1024 * 1024)
+#define MICROPY_HW_QSPIFLASH_CS             (pin_B6)
+#define MICROPY_HW_QSPIFLASH_SCK            (pin_B2)
+#define MICROPY_HW_QSPIFLASH_IO0            (pin_D11)
+#define MICROPY_HW_QSPIFLASH_IO1            (pin_D12)
+#define MICROPY_HW_QSPIFLASH_IO2            (pin_E2)
+#define MICROPY_HW_QSPIFLASH_IO3            (pin_D13)
+
+// SPI flash #1, block device config
+extern const struct _mp_spiflash_config_t spiflash_config;
+extern struct _spi_bdev_t spi_bdev;
+#define MICROPY_HW_BDEV_SPIFLASH    (&spi_bdev)
+#define MICROPY_HW_BDEV_SPIFLASH_CONFIG (&spiflash_config)
+#define MICROPY_HW_BDEV_SPIFLASH_SIZE_BYTES (MICROPY_HW_SPIFLASH_SIZE_BITS / 8)
+#define MICROPY_HW_BDEV_SPIFLASH_EXTENDED (&spi_bdev)
+#endif
