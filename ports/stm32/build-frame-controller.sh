672f7e835f21b0401ec9846ae4a16f8f32d02a69
fabianlefebre <117187889+fabianlefebre@users.noreply.github.com>
2024-12-12 16:59:36 +0100

    add build files

---
+++ ports/stm32/build-frame-controller.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Build the frame controller
+
+rm -Rf build-ME_FRAME_CONTROLLER_H743 2>/dev/null || true
+
+
+mkdir -p boards/ME_FRAME_CONTROLLER_H743
+cp -r ../../../reerdigung-frame-controller/build-files/ME_FRAME_CONTROLLER_H743/ boards/ME_FRAME_CONTROLLER_H743/
+
+make BOARD=ME_FRAME_CONTROLLER_H743
