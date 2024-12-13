c634771274e4d2d51f6c13f256a7a114225e1677
Danilo Ješić <mutant.krofna@gmail.com>
2024-12-13 10:50:40 +0100

    bash copy fix

--- ports/stm32/build-cocoon-controller.sh
+++ ports/stm32/build-cocoon-controller.sh
@@ -6,6 +6,6 @@ rm -Rf build-ME_COCOON_CONTROLLER_F405 2>/dev/null || true
 
 
 mkdir -p boards/ME_COCOON_CONTROLLER_F405
-cp -r ../../../reerdigung-cocoon-controller/build-files/ME_COCOON_CONTROLLER_F405/ boards/ME_COCOON_CONTROLLER_F405/
+cp -r ../../../reerdigung-cocoon-controller/build-files/ME_COCOON_CONTROLLER_F405/* boards/ME_COCOON_CONTROLLER_F405/
 
 make BOARD=ME_COCOON_CONTROLLER_F405
