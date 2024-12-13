403f4674faf128684fbac121b753334fa69ff677
Danilo Ješić <mutant.krofna@gmail.com>
2024-12-13 10:50:40 +0100

    bash copy fix

--- ports/stm32/build-frame-controller.sh
+++ ports/stm32/build-frame-controller.sh
@@ -6,6 +6,7 @@ rm -Rf build-ME_FRAME_CONTROLLER_H743 2>/dev/null || true
 
 
 mkdir -p boards/ME_FRAME_CONTROLLER_H743
-cp -r ../../../reerdigung-frame-controller/build-files/ME_FRAME_CONTROLLER_H743/ boards/ME_FRAME_CONTROLLER_H743/
+cp -r ../../../reerdigung-frame-controller/build-files/ME_FRAME_CONTROLLER_H743/* boards/ME_FRAME_CONTROLLER_H743/
 
 make BOARD=ME_FRAME_CONTROLLER_H743
+
