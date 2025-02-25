5d75927f577cdc78ff10333943bd9742d4b2f3af
fabianlefebre <117187889+fabianlefebre@users.noreply.github.com>
2025-02-25 23:17:38 +0100

    code to build cocoon firmware

--- ports/stm32/build-cocoon-controller.sh
+++ ports/stm32/build-cocoon-controller.sh
@@ -1,11 +1,26 @@
-#! /bin/bash
+#!/bin/bash
 
-# Build the frame controller
+# Build the cocoon controller
+
+# Get commit hash of the current repository (BASE)
+current_commit_hash=$(git rev-parse HEAD)
+echo "Current repository commit hash: $current_commit_hash"
+
+# Determine commit hash of the "reerdigung-cocoon-controller" repository (COCOON)
+if [ -d "../../../reerdigung-cocoon-controller" ]; then
+    pushd ../../../reerdigung-cocoon-controller > /dev/null
+    reerdigung_commit_hash=$(git rev-parse HEAD)
+    popd > /dev/null
+    echo "reerdigung-cocoon-controller repository commit hash: $reerdigung_commit_hash"
+else
+    echo "Directory '../../../reerdigung-cocoon-controller' does not exist."
+fi
 
 rm -Rf build-ME_COCOON_CONTROLLER_F405 2>/dev/null || true
-
 
 mkdir -p boards/ME_COCOON_CONTROLLER_F405
 cp -r ../../../reerdigung-cocoon-controller/build-files/ME_COCOON_CONTROLLER_F405/* boards/ME_COCOON_CONTROLLER_F405/
 
-make BOARD=ME_COCOON_CONTROLLER_F405
+# Pass the commit hashes via CFLAGS
+make BOARD=ME_COCOON_CONTROLLER_F405 \
+     CFLAGS_EXTRA="-DMICROPY_ME_COCOON_GIT_HASH='\"${reerdigung_commit_hash}\"' -DMICROPY_ME_BASE_GIT_HASH='\"${current_commit_hash}\"'"
